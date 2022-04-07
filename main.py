dedd89cac260cb68b0e886a01f37de7b04c30cd3
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-04-08 00:04:55 +0900

    スペースをタブに統一

--- main.py
+++ main.py
@@ -10,53 +10,53 @@ from gdraw import *
 from title import *
 
 def main(game_dir):
-    res_dir = os.path.join(game_dir, "res")
-    music_dir = os.path.join(game_dir, "music")
-    usr_dir = os.path.join(game_dir, "usr")
-    if not os.path.exists(usr_dir):
-        os.mkdir(usr_dir)
-    se = {}
-    for (id, file, vol) in [("selmusic", "se_selmusic.wav", 50),
-            ("selhard", "se_selhard.wav", 30),
-            ("auto0", "se_auto0.wav", 30),
-            ("auto1", "se_auto1.wav", 30),
-            ("start", "se_start.wav", 40),
-            ("quit", "se_quit.wav", 40),
-            ("combo", "se_combo.wav", 30),
-            ("star", "se_star.wav", 30),
-            ("result0", "se_result0.wav", 40),
-            ("result1", "se_result1.wav", 40),
-            ("result2", "se_result2.wav", 40),
-            ("result3", "se_result3.wav", 40),
-            ("newscore", "se_newscore.wav", 40),
-            ("title", "se_title.wav", 50)]:
-        se[id] = pygame.mixer.Sound(os.path.join(res_dir, file))
-        se[id].set_volume(vol / 100)
+	res_dir = os.path.join(game_dir, "res")
+	music_dir = os.path.join(game_dir, "music")
+	usr_dir = os.path.join(game_dir, "usr")
+	if not os.path.exists(usr_dir):
+		os.mkdir(usr_dir)
+	se = {}
+	for (id, file, vol) in [("selmusic", "se_selmusic.wav", 50),
+			("selhard", "se_selhard.wav", 30),
+			("auto0", "se_auto0.wav", 30),
+			("auto1", "se_auto1.wav", 30),
+			("start", "se_start.wav", 40),
+			("quit", "se_quit.wav", 40),
+			("combo", "se_combo.wav", 30),
+			("star", "se_star.wav", 30),
+			("result0", "se_result0.wav", 40),
+			("result1", "se_result1.wav", 40),
+			("result2", "se_result2.wav", 40),
+			("result3", "se_result3.wav", 40),
+			("newscore", "se_newscore.wav", 40),
+			("title", "se_title.wav", 50)]:
+		se[id] = pygame.mixer.Sound(os.path.join(res_dir, file))
+		se[id].set_volume(vol / 100)
 
-    ddraw = DDraw(res_dir)
-    dtitle = DTitle(res_dir, ddraw, se)
-    while True:
-        dtitle.main()
-        if dtitle.quit:
-            break
-        dselect = DSelect(res_dir, music_dir, usr_dir, ddraw, se)
-        while not dselect.quit:
-            dselect.main()
-            if dselect.selected_item is not None:
-                dgame = DGame(ddraw, dselect.selected_item, dselect.ex, dselect.auto, se)
-                dgame.main()
+	ddraw = DDraw(res_dir)
+	dtitle = DTitle(res_dir, ddraw, se)
+	while True:
+		dtitle.main()
+		if dtitle.quit:
+			break
+		dselect = DSelect(res_dir, music_dir, usr_dir, ddraw, se)
+		while not dselect.quit:
+			dselect.main()
+			if dselect.selected_item is not None:
+				dgame = DGame(ddraw, dselect.selected_item, dselect.ex, dselect.auto, se)
+				dgame.main()
 
 if __name__ == "__main__":
-    #pygame.mixer.pre_init(44100, -16, 2, 64)
-    #pygame.mixer.quit()
-    #pygame.mixer.init(44100, -16, 2, 64)
-    pygame.init()
-    pygame.mixer.init(44100, -16, 2, 64)
-    print(sys.executable)
-    if getattr(sys, "frozen", False):
-       datadir = os.path.dirname(sys.executable)
-    else:
-       datadir = os.path.dirname(__file__)
-    main(datadir)
+	#pygame.mixer.pre_init(44100, -16, 2, 64)
+	#pygame.mixer.quit()
+	#pygame.mixer.init(44100, -16, 2, 64)
+	pygame.init()
+	pygame.mixer.init(44100, -16, 2, 64)
+	print(sys.executable)
+	if getattr(sys, "frozen", False):
+	   datadir = os.path.dirname(sys.executable)
+	else:
+	   datadir = os.path.dirname(__file__)
+	main(datadir)
 
-    pygame.quit()
+	pygame.quit()
--- sounddev.py
+++ sounddev.py
@@ -4,42 +4,42 @@ import soundfile as sf
 from scipy import signal
 
 class DSoundFile:
-    def __init__(self, dsdev, filename):
-        print("DSF init")
-        (self.data, self.fs) = sf.read(filename, dtype='float32')
-        if self.data.shape[0] > 100:
-            self.data = self.data.transpose()
-        if self.data.shape[0] < 2:
-            self.data = np.resize(self.data, (2, self.data.shape[0]))
-        print("DSF resample")
-        self.data = np.array([signal.resample(self.data[i], int(self.data.shape[1] / self.fs * 44100)) for i in (0, 1)], dtype='float32')
-        print("DSF resample finisied")
-        self.data = self.data.transpose()
-        self.dsdev = dsdev
-    def play(self):
-        self.dsdev.wavs.append(DSoundWav(self))
+	def __init__(self, dsdev, filename):
+		print("DSF init")
+		(self.data, self.fs) = sf.read(filename, dtype='float32')
+		if self.data.shape[0] > 100:
+			self.data = self.data.transpose()
+		if self.data.shape[0] < 2:
+			self.data = np.resize(self.data, (2, self.data.shape[0]))
+		print("DSF resample")
+		self.data = np.array([signal.resample(self.data[i], int(self.data.shape[1] / self.fs * 44100)) for i in (0, 1)], dtype='float32')
+		print("DSF resample finisied")
+		self.data = self.data.transpose()
+		self.dsdev = dsdev
+	def play(self):
+		self.dsdev.wavs.append(DSoundWav(self))
 class DSoundWav:
-    def __init__(self, dsf):
-        self.data = dsf.data
-        self.current = 0
-        self.playing = True
+	def __init__(self, dsf):
+		self.data = dsf.data
+		self.current = 0
+		self.playing = True
 class DSoundDev:
-    def __init__(self):
-        self.wavs = []
-        self.stream = sd.OutputStream(samplerate=44100, blocksize=44100//60, latency=0, channels=2, callback=self.sd_callback)
-        self.stream.start()
-    def loadfile(self, filename):
-        return DSoundFile(self, filename)
-    def sd_callback(self, outdata, frames, time, status):
-        if status:
-            print(status)
-        print("frames: {}".format(frames))
-        mixdata = np.array([(0,0) for _ in range(frames)], dtype='float32')
-        for dsw in self.wavs:
-            if dsw.playing:
-                chunksize = min(len(dsw.data) - dsw.current, frames)
-                mixdata[:chunksize] += dsw.data[dsw.current: dsw.current + chunksize]
-                dsw.current += chunksize
-                if chunksize < frames:
-                    dsw.playing = False
-        outdata[:] = mixdata
+	def __init__(self):
+		self.wavs = []
+		self.stream = sd.OutputStream(samplerate=44100, blocksize=44100//60, latency=0, channels=2, callback=self.sd_callback)
+		self.stream.start()
+	def loadfile(self, filename):
+		return DSoundFile(self, filename)
+	def sd_callback(self, outdata, frames, time, status):
+		if status:
+			print(status)
+		print("frames: {}".format(frames))
+		mixdata = np.array([(0,0) for _ in range(frames)], dtype='float32')
+		for dsw in self.wavs:
+			if dsw.playing:
+				chunksize = min(len(dsw.data) - dsw.current, frames)
+				mixdata[:chunksize] += dsw.data[dsw.current: dsw.current + chunksize]
+				dsw.current += chunksize
+				if chunksize < frames:
+					dsw.playing = False
+		outdata[:] = mixdata
